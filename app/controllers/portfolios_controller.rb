afe6540c7f3edb143cbf601fea3aaa476f4c1a5f
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-03-05 12:32:07 -0600

    Add index action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
+# portfolio actions
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def show; end
 end

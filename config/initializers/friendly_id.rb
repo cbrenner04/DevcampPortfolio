4445daaf8bf14e202958c4d09561b89f787d3c78
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-03-05 19:45:51 -0600

    Add rspec and factory girl

--- config/initializers/friendly_id.rb
+++ config/initializers/friendly_id.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # FriendlyId Global Configuration
 #
 # Use this to set up shared configuration options for your entire application.
@@ -17,7 +18,7 @@ FriendlyId.defaults do |config|
   config.use :reserved
 
   config.reserved_words = %w(new edit index session login logout users admin
-    stylesheets assets javascripts images)
+                             stylesheets assets javascripts images)
 
   #  ## Friendly Finders
   #
@@ -52,16 +53,16 @@ FriendlyId.defaults do |config|
   #
   # config.slug_column = 'slug'
   #
-  # When FriendlyId can not generate a unique ID from your base method, it appends
-  # a UUID, separated by a single dash. You can configure the character used as the
-  # separator. If you're upgrading from FriendlyId 4, you may wish to replace this
-  # with two dashes.
+  # When FriendlyId can not generate a unique ID from your base method, it
+  # appends a UUID, separated by a single dash. You can configure the character
+  # used as the separator. If you're upgrading from FriendlyId 4, you may wish
+  # to replace this with two dashes.
   #
   # config.sequence_separator = '-'
   #
   # Note that you must use the :slugged addon **prior** to the line which
-  # configures the sequence separator, or else FriendlyId will raise an undefined
-  # method error.
+  # configures the sequence separator, or else FriendlyId will raise an
+  # undefined method error.
   #
   #  ## Tips and Tricks
   #
---
+++ spec/factories/blogs.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+FactoryGirl.define do
+  factory :blog do
+    title 'Foo'
+    body 'Bar'
+    slug 'foo'
+    status 'draft'
+  end
+end

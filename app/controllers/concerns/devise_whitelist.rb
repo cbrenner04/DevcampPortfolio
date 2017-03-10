2beb9c76701f21c708bf9cf37126c2b7b21fe152
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-03-09 19:29:18 -0600

    Added controller concerns for devise params

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+# whitelist custom params for devise
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :configure_permitted_parameters, if: :devise_controller?
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+end

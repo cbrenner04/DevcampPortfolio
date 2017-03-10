f883d5db27d68fea45c42998d2a758fae641aa77
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-03-09 19:09:56 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               sign_up: 'register'
+             }
+
   resources :whatevers
   resources :blogs do
     get :toggle_status, on: :member

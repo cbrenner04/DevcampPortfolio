e79979cf0935839a28ecdd038380bbda08102716
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-03-13 22:34:08 -0500

    Add about me image

--- db/schema.rb
+++ db/schema.rb
@@ -91,6 +91,12 @@ ActiveRecord::Schema.define(version: 20170313213630) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "whatevers", force: :cascade do |t|
+    t.string   "whatever"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "blogs", "topics"
   add_foreign_key "technologies", "portfolios"
 end

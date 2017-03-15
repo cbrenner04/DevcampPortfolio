9f75f817e97eef07d7f0c76ff0a2bd3826d76bdc
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-03-15 01:30:10 -0500

    Implemented ActionCable for comments

---
+++ app/channels/blogs_channel.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+# channel for comments on blogs
+class BlogsChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "blogs_#{params['blog_id']}_channel"
+  end
+
+  def unsubscribed; end
+
+  def send_comment(data)
+    current_user
+      .comments
+      .create(
+        content: data['comment'],
+        blog_id: data['blog_id']
+      )
+  end
+end

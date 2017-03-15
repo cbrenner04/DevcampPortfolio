13046b4e2d91fc403b72294a2a1a33cc4c8c11c4
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-03-15 01:51:05 -0500

    Completed comments with action cable

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 # no doc
 module BlogsHelper
+  def gravatar_helper(user)
+    image_tag(
+      "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}",
+      width: 40
+    )
+  end
 end

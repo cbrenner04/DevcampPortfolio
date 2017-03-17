53535d678e47bd128c709761c00e8621a5430e5f
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-03-16 23:02:11 -0500

    Add skills in about and updates styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,11 @@
 class PagesController < ApplicationController
   def home
     @posts = Blog.all
-    @skills = Skill.all
   end
 
-  def about; end
+  def about
+    @skills = Skill.descending
+  end
 
   def contact; end
 
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,4 +2,8 @@
 # no doc
 class Skill < ApplicationRecord
   validates :title, :percent_utilized, presence: true
+
+  def self.descending
+    order(percent_utilized: :desc)
+  end
 end
